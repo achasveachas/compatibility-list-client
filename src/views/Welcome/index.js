0672ced7138b7a4bf9c7a1adf711d9c098e4424a
Yechiel Kalmenson <contact@yechiel.me>
2017-08-02 15:20:51 -0400

    Updated the welcome screen

--- src/views/Welcome/index.js
+++ src/views/Welcome/index.js
@@ -2,8 +2,9 @@ import React from 'react'
 
 const  Welcome = () =>
     <div className="uk-position-center">
-      <h1 className="uk-heading-line uk-text-center"><span>Welcome To My Job Application Tracker</span></h1>
-      <h3 className="uk-heading-line uk-text-center"><span>Please <a className="uk-link-muted" href='/signup'>sign up</a> or <a className="uk-link-muted" href='/login'>log in</a> to use</span></h3>
+      <h1 className="uk-heading-line uk-text-center"><span>Software Compatibility Tracker</span></h1>
+      <h3 className="uk-heading-line uk-text-center"><span>Please <a className="uk-link-muted" href='/login'>log in</a></span></h3>
+      <h4 className="uk-heading-line uk-text-center"><span>If you do not have an account please contact your manager.</span></h4>
     </div>
 
 export default Welcome
