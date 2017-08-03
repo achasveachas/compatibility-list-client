d710ae14a625ad02b53f2bdc305ea5effb57f541
Yechiel Kalmenson <contact@yechiel.me>
2017-08-03 13:05:02 -0400

    updated the rows of the application table

--- src/views/components/ApplicationRow/index.js
+++ src/views/components/ApplicationRow/index.js
@@ -7,23 +7,19 @@ function ApplicationRow(props){
       props.onDelete(props.application.id)
     }
   }
-  let contact = ""
-  if (props.application.contact_name && props.application.contact_title) {
-    contact = props.application.contact_name + " - " + props.application.contact_title
-  } else if (props.application.contact_name) {
-    contact = props.application.contact_name
-  } else if (props.application.contact_title) {
-    contact = props.application.contact_title
-  }
+
   return (
         <tr>
-          <td onClick={handleClick}>{props.application.company}</td>
-          <td onClick={handleClick}>{props.application.job_title}</td>
-          <td onClick={handleClick}>{contact}</td>
-          <td onClick={handleClick}>{props.application.date}</td>
-          <td onClick={handleClick}>{props.application.action}</td>
+          <td onClick={handleClick}>{props.application.software}</td>
+          <td onClick={handleClick}>{props.application.gateway}</td>
+          <td onClick={handleClick}>{props.application.omaha ? <span>&#x2713;</span> : <span> </span>}</td>
+          <td onClick={handleClick}>{props.application.nashville ? <span>&#x2713;</span> : <span> </span>}</td>
+          <td onClick={handleClick}>{props.application.north ? <span>&#x2713;</span> : <span> </span>}</td>
+          <td onClick={handleClick}>{props.application.buypass ? <span>&#x2713;</span> : <span> </span>}</td>
+          <td onClick={handleClick}>{props.application.elavon ? <span>&#x2713;</span> : <span> </span>}</td>
+          <td onClick={handleClick}>{props.application.tsys ? <span>&#x2713;</span> : <span> </span>}</td>
+          <td onClick={handleClick}>{props.application.other ? <span>&#x2713;</span> : <span> </span>}</td>
           <td onClick={handleClick}>{props.application.notes}</td>
-          <td onClick={handleClick}>{props.application.complete ? 	<span>&#x2713;</span> :	<span>&#x2717;</span> }</td>
           <td><button className="uk-button uk-button-danger uk-button-small" onClick={handleDelete}>Delete</button></td>
         </tr>)
 }
