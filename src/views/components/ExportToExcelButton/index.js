cb4ac058544b5fcc95c5b4c748602e3e038e30ed
Yechiel Kalmenson <contact@yechiel.me>
2017-10-17 17:19:21 -0400

    add some custom styling

--- src/views/components/ExportToExcelButton/index.js
+++ src/views/components/ExportToExcelButton/index.js
@@ -3,7 +3,7 @@ import React from 'react'
 const ExportToExcelButton = (props) => {
   return (
     <button
-    className="uk-button uk-button-secondary uk-margin-medium uk-margin-left"
+    className="uk-button uk-button-green uk-margin-medium uk-margin-left"
     onClick={props.onClick}>
       Export To Excel
     </button>
