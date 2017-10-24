28d2f9b96b43b12c0920de167a61e1e8e61592c8
Yechiel Kalmenson <contact@yechiel.me>
2017-10-24 14:26:26 -0400

    remove click handler from applicationRow and left it only on the view button

--- src/views/components/ApplicationRow/index.js
+++ src/views/components/ApplicationRow/index.js
@@ -6,16 +6,16 @@ function ApplicationRow(props){
 
   return (
         <tr>
-          <td onClick={handleClick}>{props.application.software}</td>
-          <td onClick={handleClick}>{props.application.gateway}</td>
-          <td onClick={handleClick}>{props.application.omaha ? <span>&#x2713;</span> : <span> </span>}</td>
-          <td onClick={handleClick}>{props.application.nashville ? <span>&#x2713;</span> : <span> </span>}</td>
-          <td onClick={handleClick}>{props.application.north ? <span>&#x2713;</span> : <span> </span>}</td>
-          <td onClick={handleClick}>{props.application.buypass ? <span>&#x2713;</span> : <span> </span>}</td>
-          <td onClick={handleClick}>{props.application.elavon ? <span>&#x2713;</span> : <span> </span>}</td>
-          <td onClick={handleClick}>{props.application.tsys ? <span>&#x2713;</span> : <span> </span>}</td>
-          <td onClick={handleClick}>{props.application.other ? <span>&#x2713;</span> : <span> </span>}</td>
-          <td onClick={handleClick}>{props.application.notes}</td>
+          <td>{props.application.software}</td>
+          <td>{props.application.gateway}</td>
+          <td>{props.application.omaha ? <span>&#x2713;</span> : <span> </span>}</td>
+          <td>{props.application.nashville ? <span>&#x2713;</span> : <span> </span>}</td>
+          <td>{props.application.north ? <span>&#x2713;</span> : <span> </span>}</td>
+          <td>{props.application.buypass ? <span>&#x2713;</span> : <span> </span>}</td>
+          <td>{props.application.elavon ? <span>&#x2713;</span> : <span> </span>}</td>
+          <td>{props.application.tsys ? <span>&#x2713;</span> : <span> </span>}</td>
+          <td>{props.application.other ? <span>&#x2713;</span> : <span> </span>}</td>
+          <td>{props.application.notes}</td>
           <td onClick={handleClick}><NavLink to="/application" className="uk-button uk-button-default">View</NavLink></td>
         </tr>)
 }
